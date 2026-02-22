6c5c5dd9a5fe3106a31e839d1480801d655519c0
anurag0304-stack <anurag720870@gmail.com>
2026-02-22 16:13:17 +0530

    Update KNN1.py

--- KNN1.py
+++ KNN1.py
@@ -2,22 +2,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
-x = np.array([[1, 2], [2, 3], [3, 1], [6, 5], [7, 7], [8, 6]])
-y = np.array([0, 0, 0, 1, 1, 1])
+x = np.array([[30, 75], [25, 80], [27, 60], [31, 65], [23, 85], [28, 65]])
+y = np.array([0, 1, 0, 1, 0, 1])
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(x, y)
-new_point = np.array([[4, 4]])
+new_point = np.array([[26, 78]])
 prediction = knn.predict(new_point)[0]
 plt.figure(figsize=(8, 6))
-plt.scatter(x[y==0,0], x[y==0,1], c='blue', label='Sunny',s=100)
-plt.scatter(x[y==1,0], x[y==1,1], c='red', label='Rainy',s=100)
+plt.scatter(x[y==0,0], x[y==0,1], label='Sunny',s=100)
+plt.scatter(x[y==1,0], x[y==1,1], label='Rainy',s=100)
 plt.scatter(
     new_point[0, 0],
     new_point[0, 1],
     marker="*",
-    s=200,
+    s=300,
     label='New Prediction',
-    c='green'
+    c='red'
     )
 plt.xlabel('temperture(°C)')
 plt.ylabel('humidity(%)')
@@ -28,4 +28,5 @@ plt.show()
 if prediction == 0:
     print("The predicted weather condition for the new point is: Sunny")
 else:
+
     print("The predicted weather condition for the new point is: Rainy")
