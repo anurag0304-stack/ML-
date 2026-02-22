c493638b2553601acfd23188a5dc096e1ef8d0b6
anurag0304-stack <anurag720870@gmail.com>
2026-02-22 16:20:51 +0530

    Refactor KNN1.py for improved readability

--- KNN1.py
+++ KNN1.py
@@ -9,7 +9,7 @@ knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(x, y)
 new_point = np.array([[26, 78]])
 prediction = knn.predict(new_point)[0]
-plt.figure(figsize=(8, 6))
+plt.figure(figsize=(6,8))
 plt.scatter(x[y==0,0], x[y==0,1], label='Sunny',s=100)
 plt.scatter(x[y==1,0], x[y==1,1], label='Rainy',s=100)
 plt.scatter(
@@ -31,4 +31,5 @@ if prediction == 0:
 else:
 
     print("The predicted weather condition for the new point is: Rainy")
+
 
